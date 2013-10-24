8024f73d51fa3681f6b63b3b462f7238269ec602
Allen Turner <tedturner29@ufl.edu>
2013-10-24 14:19:05 -0400

    added individual serversockets

--- Peer.java
+++ Peer.java
@@ -6,19 +6,23 @@ import java.net.*;
 //import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 
 
 public class Peer {
-	private static int [] peerPorts = { 1001, 1002, 1003, 1004, 1005, 1006 };
+	private static int [] peerPorts = { 11, 12, 13, 14, 15, 16 };
 	// private static int nextPort = 0;
 	private int port;
-	private ServerSocket sev;
-	private Socket soc;
-	private ArrayList<Socket> myPeersSev;
-	private ArrayList<Socket> myPeersCli;
+	private String peerId;
+	//private ServerSocket sev;
+	//private Socket soc;
+	private ArrayList<Socket> myPeers;
+	private ServerSocket [] myServers;
+	//private ArrayList<Socket> myPeersCli;
 	private boolean [] connected;
 	private int numConnectedSev;
+	private HashMap<String, Integer> mapPeers = new HashMap<String, Integer>();
 	public Peer (String peerId) {
 		//System.out.println("Test");
 		try {
@@ -26,16 +30,23 @@ public class Peer {
 			// peerPorts.add(port);
 			// nextPort++;
 			port = Integer.parseInt(peerId);
-			sev = new ServerSocket(port);
+			this.peerId = peerId;
+			//sev = new ServerSocket(port);
 			//sev.setSoTimeout(100);
-			soc = new Socket();
-			myPeersSev = new ArrayList<Socket>();
-			myPeersCli = new ArrayList<Socket>();
+			//soc = new Socket();
+			myServers = new ServerSocket[5];
+			
+			myPeers = new ArrayList<Socket>();
+			//myPeersCli = new ArrayList<Socket>();
 			numConnectedSev = 1;
 			connected = new boolean [peerPorts.length];
+			int k = 0;
 			for (int i = 0; i < peerPorts.length; i++) {
 				if (peerPorts[i] == port) {
 					connected[i] = true;
+				}
+				else {
+					myServers[k++] = new ServerSocket(createAddressPort(peerPorts[i]));
 				}
 			}
 		}
@@ -53,10 +64,10 @@ public class Peer {
 			for (int i = 0; i < peerPorts.length; i++) { // && !soc.isConnected(); i++) {
 				System.out.print("PortMe: " + port + " To Port: " + peerPorts[i] );
 				if (!connected[i]) {
-					myPeersSev.add(new Socket("localhost", peerPorts[i]));
+					myPeers.add(new Socket("localhost", createToPort(peerPorts[i])));
 					//soc.connect(new InetSocketAddress("localhost", peerPorts[i]));\=
 					System.out.println(" Worked!");
-					System.out.println("Socket: " + myPeersSev.get(myPeersSev.size()-1).toString());
+					System.out.println("Socket: " + myPeers.get(myPeers.size()-1).toString());
 					connected[i] = true;
 				}
 				else {
@@ -74,12 +85,14 @@ public class Peer {
 	public void accept() {
 		if (numConnectedSev < peerPorts.length) { 
 			try {
-				System.out.println("Waiting");
-				Socket p = sev.accept();
-				if (!myPeersCli.contains(p)) {
-					myPeersCli.add(p);
+				for (int i = 0; i < myServers.length; i++) {
+					System.out.println("Server " + myServers[i] + " is Waiting");
+					myServers[i].accept();
+					//if (!myPeers.contains(p)) {
+						//myPeersCli.add(p);
 					numConnectedSev++;
 				}
+				//}
 			}
 			catch (Exception e) {
 				e.printStackTrace();
@@ -102,20 +115,42 @@ public class Peer {
 		}
 		return true;
 	}
-	// public void sendToClient(String message) {
-		// DataOutputStream out = new DataOutputStream(client.getOutputStream());
+	// public void sendHeaderToPeer(int index) {
+		// Socket p = myPeers[index];
+		// byte[] message = new byte[32];
+		// message[0] = 'H';
+		// message[1] = 'E';
+		// message[2] = 'L';
+		// message[3] = 'L';
+		// message[4] = 'O';
+		// DataOutputStream out = new DataOutputStream(p.getOutputStream());
+		// out.write(message, 0, 32);
+		// out.write(peerId.getBytes(), 0, 4);
+	// }
+	// public void receiveHeaderFromPeer(int index) {
+		// Socket p = myPeers[index];
+		// byte[] message;
+		// DataInputStream in = new DataInputStream(p.getInputStream());
+		// in.read(message, 0, 32);
+		// byte [] id;
+		// in.read(id, 0, 4);
+		// String pId = new String(id);
+		// mapPeers.put(pId, index);
+	// }
+	// public void sendToPeer(String message, String peerId) {
+		// Socket p = myPeers[mapPeers.get(peerId)];
+		// DataOutputStream out = new DataOutputStream(p.getOutputStream());
 		// out.writeUTF(message);
 	// }
-	// public String receiveFromClient() {
-		// DataInputStream in = new DataInputStream(client.getInputStream());
+	// public String receiveFromPeer(String peerId) {
+		// Socket p = myPeers[mapPeers.get(peerId)];
+		// DataInputStream in = new DataInputStream(p.getInputStream());
 		// return in.readLine();
 	// }
-	// public void sendToServer(String message) {
-		// DataOutputStream out = new DataOutputStream(soc.getOutputStream());
-		// out.writeUTF(message);
-	// }
-	// public String receiveFromServer() {
-		// DataInputStream in =  new DataInputStream(soc.getInputStream());
-		// return in.readLine();
-	// }
+	public int createToPort(int port) {
+		return Integer.parseInt(port+peerId);
+	}
+	public int createAddressPort(int port) {
+		return Integer.parseInt(peerId+port);
+	}
 }
